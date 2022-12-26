e76c48d2b72faee76f19a27c075e391cf0763e81
안지인 / Jiin An <00.jiin.an@gmail.com>
2022-12-26 23:10:58 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,8 +1,9 @@
 A, B, C = map(int, input().split())
-import statistics as stat
-if A == B and B == C:
+if A == B == C:
     print(10000 + A * 1000)
-elif A != B and A != C and B != C:
-    print(max(A, B, C) * 100)
+elif A == B or A == C:
+    print(1000 + A * 100)
+elif B == C:
+    print(1000 + B * 100)
 else:
-    print(1000 + stat.mode([A, B, C]) * 100)
+    print(max(A, B, C) * 100)
