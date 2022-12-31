7b20105716a0397bd5ff0304e8ba67bbf219a870
안지인 / Jiin An <00.jiin.an@gmail.com>
2023-01-01 08:54:34 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,6 +1,3 @@
-answer = []
 def solution(num_list):
-    for i in range(len(num_list)):
-        i  = num_list[len(num_list) - 1 - i]
-        answer.append(i)
+    answer = num_list[::-1]
     return answer
